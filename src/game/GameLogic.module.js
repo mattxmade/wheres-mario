cb1d40a6d5da0dd96f9f1d34c93cd3902576f3e4
Matt <97047080+mattxmade@users.noreply.github.com>
2023-01-14 12:18:34 +0000

    Refactor module, abstract level functions to seperate module

--- src/game/GameLogic.module.js
+++ src/game/GameLogic.module.js
@@ -1,90 +1,11 @@
-import getNpcList from "./NPCs";
+import characters from "./characters";
 import GameAssets from "./GameAssets.module";
 import BoardMaps from "./BoardMaps.module";
 
+import LevelBuilder from "./LevelBuilder.module";
+import gameMessages from "./GameMessages";
+
 const GameLogic = (() => {
-  const levelTotal = 6;
-  const gameItems = GameAssets.generateAssetBanks("find");
-
-  const randomItem = (array) => array[Math.floor(Math.random() * array.length)];
-
-  const shuffleItems = (array) => {
-    return randomItem(array);
-  };
-
-  const requestGameItems = (array, numOfItems) =>
-    [...new Array(numOfItems)].map(() => randomItem(array));
-
-  const getLevelMap = (num) => GameAssets.getLevelMap(num);
-
-  const requestLevelItems = (gameLevel) => {
-    let itemCount;
-    let itemProperty;
-    let itemReference;
-
-    switch (gameLevel) {
-      case 1:
-        itemProperty = "name";
-        itemReference = "Big Mario";
-        break;
-
-      case 2:
-        itemProperty = "name";
-        itemReference = "Magikoopa";
-        break;
-
-      case 3:
-        itemProperty = "name";
-        itemReference = "Toad";
-        break;
-
-      case 4:
-        itemProperty = "name";
-        itemReference = "Big Boo";
-        break;
-
-      case 5:
-        itemProperty = "name";
-        itemReference = "Lemmy";
-        break;
-
-      case 6:
-        itemProperty = "name";
-        itemReference = "Mario";
-        break;
-    }
-
-    itemCount = BoardMaps.requestItemMapCount(
-      gameLevel,
-      itemReference,
-      itemProperty
-    );
-
-    // console.log(itemCount);
-
-    return {
-      count: itemCount ?? 1,
-      group: itemReference,
-      assets: gameItems.filter((item) => item.name.includes(itemReference)),
-    };
-  };
-
-  const levelFactory = (num) => {
-    if (typeof num !== "number" && num < 1 && num > levelTotal) return 1;
-
-    return {
-      map: getLevelMap(num),
-      items: requestLevelItems(num),
-      validator: BoardMaps.checkMapPosition,
-    };
-  };
-
-  const applyPowerUp = () => {};
-  const applyPenalty = () => {};
-  const updateScore = () => {};
-  const updateLevel = () => {};
-  const gameResult = () => {};
-
   const shuffleIndexOrder = (array, stopper) => {
     const shuffledArray = [];
     let uniqueIndex = 0;
@@ -107,26 +28,16 @@ const GameLogic = (() => {
     return shuffledArray.map((val) => array[val]);
   };
 
-  const npcList = [
-    "Bowser",
-    "Bowser Jr",
-    "Peach",
-    "Yoshi",
-    "Wiggler",
-    "Waddlewing",
-    "Cooligan",
-    "Bullet Bill",
-    "Big Urchin",
-    "Cheep Cheep",
-    "Koopa",
-  ];
+  const levelTotal = 6;
+  const npcList = characters;
+  const { gameItems, levelFactory } = LevelBuilder;
 
   return {
     npcList,
     gameItems,
     levelTotal,
     levelFactory,
-    requestGameItems,
+    gameMessages,
     shuffleIndexOrder,
   };
 })();
